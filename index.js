8eeef94b82f844209f95f43bc0e89472fae27cea
iamLolade <adeniyiotemade@gmail.com>
2022-02-24 03:04:46 +0100

    Isograms Solution

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ const filter_list = (array) => {
 }
 
 console.log(filter_list([1,2,'a','b']));
+
+
+// Q. Isograms | Kata 7
+
+// Steps
+// Create empty object to keep track of character occurrence
+// Convert word to lowercase
+// Loop through word
+// Get array of object values
+// Loop through values to confirm number of occurrence
+
+function isIsogram(str){
+    //...
+    let map = {};
+    let word = str.toLowerCase();
+    
+    for(let i = 0; i < word.length; i++) {
+      let char = word[i] 
+      if(map[char]) {
+        map[char]++;
+      } else {
+        map[char] = 1;
+      }
+    }
+    let countArray = Object.values(map);
+    return countArray.every(elem => elem === 1);
+}
+
+console.log(isIsogram("Occurrence"))
